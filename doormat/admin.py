5bd085d84d99ffb2efb2935752ede1a14621fecf
Jeremy Carbaugh <jcarbaugh@gmail.com>
2011-05-25 17:04:45 -0400

    make is_enabled list editable

--- doormat/admin.py
+++ doormat/admin.py
@@ -4,6 +4,7 @@ from doormat.models import DoorMat
 class DoorMatAdmin(admin.ModelAdmin):
     list_display = ('domain', 'path', 'is_enabled', 'last_published')
     list_display_links = ('domain', 'path')
+    list_editable = ('is_enabled',)
     list_filter = ('is_enabled', 'domain')
     readonly_fields = ('last_published',)
     search_fields = ('domain', 'path', 'content')
